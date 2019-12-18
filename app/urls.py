c48a6a3c9daa1cad35a99e4108db81ff4a591047
Eddie Pease <eddiepease1@gmail.com>
2019-12-18 11:07:08 +0000

    feat: add config file for Azure creds

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 """This module returns a company URL, given a company name"""
 
 import requests
+import config
 
 
 class URLFinder:
@@ -10,7 +11,7 @@ class URLFinder:
     """
 
     def __init__(self):
-        self.subscription_key = 'd91d74d8fb644cd5b08fac45691fa01d'
+        self.subscription_key = config.azure_subscription_key
         self.search_url = "https://api.cognitive.microsoft.com/bing/v7.0/search"
 
     def choose_url(self, results_json, url_count=0):
@@ -43,8 +44,12 @@ class URLFinder:
         # access API
         headers = {'Ocp-Apim-Subscription-Key': self.subscription_key}
         params = {"q": company, "textDecorations": True, "textFormat": "HTML"}
-        response = requests.get(self.search_url, headers=headers, params=params)
+        response = requests.get(self.search_url, headers=headers, params=params, timeout=10)
         results = response.json()
+
+        # error handling
+        if 'error' in results:
+            raise ValueError(results['error']['message'])
 
         # extract url from response
         url = self.choose_url(results_json=results)
