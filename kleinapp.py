5d9a54a3cd1331705371f32c9f96fa6094a3561d
Eddie Pease <eddiepease1@gmail.com>
2020-01-13 10:22:37 +0000

    doc: test documention

--- kleinapp.py
+++ kleinapp.py
@@ -27,8 +27,8 @@ def create_embedding(request, company_name):
     Finds the URL, scrapes the website, returns the embedding
 
     :param company_name: string of company name
-    
-    :return: result: json dictionary of company embedding
+
+    :return: json dictionary of company embedding
     """
 
     url = URLFinder().run(company=company_name)
